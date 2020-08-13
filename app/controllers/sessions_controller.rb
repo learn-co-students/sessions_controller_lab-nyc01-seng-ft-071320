74ecc1e27a0c6c6ab77b799383d875fb56f9586e
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-08-13 18:23:43 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    return redirect_to(controller: 'sessions',
-                       action: 'new') if !params[:name] || params[:name].empty?
+     
+    if !params[:name] || params[:name].empty? # we only redirect to the new "login" if params[:name] is nil or empty
+    redirect_to(controller: 'sessions', action: 'new') 
+    else
     session[:name] = params[:name]
     redirect_to controller: 'application', action: 'hello'
+    end
   end
 
   def destroy
